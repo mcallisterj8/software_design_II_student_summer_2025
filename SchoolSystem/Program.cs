7fc20209b450c1e5b84cb3b4b25f02cde8648e8e
Jacob McAllister <mcallisterj@westerntc.edu>
2025-05-14 13:05:01 -0500

    End of class
    Signed-off-by: Jacob McAllister <mcallisterj@westerntc.edu>

--- SchoolSystem/Program.cs
+++ SchoolSystem/Program.cs
@@ -1,10 +1,12 @@
 ﻿
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using SchoolSystem.Data;
 using SchoolSystem.Services;
 
 ServiceProvider _serviceProvider;
 SeedingService _seedingService;
+BasicQueryService _basicQueryService;
 
 // Create container to hold services for dependency injection
 var services = new ServiceCollection();
@@ -12,6 +14,7 @@ var services = new ServiceCollection();
 // Add services to the service container
 services.AddDbContext<ApplicationDbContext>();
 services.AddScoped<SeedingService>();
+services.AddScoped<BasicQueryService>();
 
 /*
     Get the service provider - this our way to take
@@ -21,7 +24,14 @@ _serviceProvider = services.BuildServiceProvider();
 
 // Retrieve instance of SeedingService from the container
 _seedingService = _serviceProvider.GetRequiredService<SeedingService>();
+_basicQueryService = _serviceProvider.GetRequiredService<BasicQueryService>();
 
 // Call method to seed the database
 await _seedingService.SeedDatabase();
 
+List<string> instrNames = 
+    await _basicQueryService.GetAllInstructorNames();
+
+foreach(string name in instrNames) {
+    Console.WriteLine($"Name: {name}");
+}
---
+++ SchoolSystem/Services/BasicQueryService.cs
@@ -0,0 +1,56 @@
+
+
+using Microsoft.EntityFrameworkCore;
+using SchoolSystem.Data;
+
+namespace SchoolSystem.Services;
+
+public class BasicQueryService {
+
+    private readonly ApplicationDbContext _context;
+
+    public BasicQueryService(ApplicationDbContext context) {
+        _context = context;
+    }
+
+    public async Task< List<string> > GetAllInstructorNames() {
+        /*
+            SELECT FirstName
+            FROM Instructors
+        */
+        return await _context.Instructors
+            .Select(instructor => instructor.FirstName)
+            .ToListAsync();
+    }
+
+    public async Task< List<string> > GetDeptNamesWithMoreThanOneCourse() {
+        /*
+            SELECT Name
+            FROM Departments
+            WHERE Departments.Courses.Count > 1
+        */
+
+        return await _context.Departments
+            .Where(dept => dept.Courses.Count > 1)
+            .Select(dept => dept.Name)
+            .ToListAsync();
+    }
+
+    public async Task<string?> GetDeptWithMostCourses() {
+        /*
+            SELECT Name
+            FROM Departments
+            ORDER BY Departments.Courses.Count DESC
+            LIMIT 1
+
+        */
+        return await _context.Departments
+            .OrderByDescending(dept => dept.Courses.Count)
+            .Select(dept => dept.Name)
+            .FirstOrDefaultAsync();
+
+    }
+
+
+
+}
