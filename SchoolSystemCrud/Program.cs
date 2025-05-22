68d54a8da7455abf87bc9d9b484d9eb92fe91340
Jacob McAllister <datadeltaanalysis@protonmail.com>
2025-05-22 12:16:04 -0500

    Class prep
    Signed-off-by: Jacob McAllister <datadeltaanalysis@protonmail.com>

---
+++ SchoolSystemCrud/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+using Microsoft.Extensions.DependencyInjection;
+using SchoolSystemCrud.Data;
+using SchoolSystemCrud.Models;
+using SchoolSystemCrud.Services;
+
+ServiceProvider _serviceProvider;
+SeedingService _seedingService;
+
+
+// Create container to hold services for dependency injection
+var services = new ServiceCollection();
+
+// Add services to service container
+services.AddDbContext<ApplicationDbContext>();
+services.AddScoped<SeedingService>();
+
+/*
+    Get the service provider - this is our way to take something
+    out of the container.
+*/
+_serviceProvider = services.BuildServiceProvider();
+
+// Retrieve instance of SeedingService from the container
+_seedingService = _serviceProvider.GetRequiredService<SeedingService>();
+
+
+await _seedingService.SeedDatabase();
