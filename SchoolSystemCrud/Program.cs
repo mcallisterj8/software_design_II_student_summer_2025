81aa32931e701be4e945bec65ae9804acb7e426f
Jacob McAllister <datadeltaanalysis@protonmail.com>
2025-05-28 09:18:48 -0500

    Update
    Signed-off-by: Jacob McAllister <datadeltaanalysis@protonmail.com>

--- SchoolSystemCrud/Program.cs
+++ SchoolSystemCrud/Program.cs
@@ -6,6 +6,7 @@ using SchoolSystemCrud.Services;
 
 ServiceProvider _serviceProvider;
 SeedingService _seedingService;
+CrudService _crudService;
 
 
 // Create container to hold services for dependency injection
@@ -14,6 +15,7 @@ var services = new ServiceCollection();
 // Add services to service container
 services.AddDbContext<ApplicationDbContext>();
 services.AddScoped<SeedingService>();
+services.AddScoped<CrudService>();
 
 /*
     Get the service provider - this is our way to take something
@@ -24,5 +26,6 @@ _serviceProvider = services.BuildServiceProvider();
 // Retrieve instance of SeedingService from the container
 _seedingService = _serviceProvider.GetRequiredService<SeedingService>();
 
+_crudService = _serviceProvider.GetRequiredService<CrudService>();
 
 await _seedingService.SeedDatabase();
