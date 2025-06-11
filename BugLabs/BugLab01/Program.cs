148a7e721e5148a58ca5cf0e5278466dd42671ce
Jacob McAllister <mcallisterj@westerntc.edu>
2025-06-11 12:15:02 -0500

    Update
    Signed-off-by: Jacob McAllister <mcallisterj@westerntc.edu>

--- BugLabs/BugLab01/Program.cs
+++ BugLabs/BugLab01/Program.cs
@@ -16,3 +16,39 @@ _serviceProvider = services.BuildServiceProvider();
 
 // Retrieve the MusicQueryService
 _musicQueryService = _serviceProvider.GetRequiredService<MusicQueryService>();
+
+Console.WriteLine("\n--- GetAllArtistsWithAlbums ---");
+var artistsWithAlbums = await _musicQueryService.GetAllArtistsWithAlbums();
+foreach (var artist in artistsWithAlbums) {
+    Console.WriteLine($"{artist.Name} ({artist.Albums.Count} albums)");
+}
+
+Console.WriteLine("\n--- GetAllArtistsWithMoreThanOneAlbum ---");
+var artistsWithMoreThanOneAlbum = await _musicQueryService.GetAllArtistsWithMoreThanOneAlbum();
+foreach (var artist in artistsWithMoreThanOneAlbum) {
+    Console.WriteLine($"{artist.Name} ({artist.Albums.Count} albums)");
+}
+
+Console.WriteLine("\n--- GetArtistByNameWithAlbums ---");
+var artistByName = await _musicQueryService.GetArtistByNameWithAlbums("AC/DC");
+Console.WriteLine(artistByName != null
+    ? $"{artistByName.Name} has {artistByName.Albums.Count} albums"
+    : "Artist not found");
+
+Console.WriteLine("\n--- GetTracksByAlbumId (AlbumId = 1) ---");
+var tracksByAlbum = await _musicQueryService.GetTracksByAlbumId(1);
+foreach (var track in tracksByAlbum) {
+    Console.WriteLine(track.Name);
+}
+
+Console.WriteLine("\n--- GetAllGenresWithTracks ---");
+var genresWithTracks = await _musicQueryService.GetAllGenresWithTracks();
+foreach (var genre in genresWithTracks) {
+    Console.WriteLine($"{genre.Name} ({genre.Tracks.Count} tracks)");
+}
+
+Console.WriteLine("\n--- GetTracksByGenreId (GenreId = 1) ---");
+var tracksByGenre = await _musicQueryService.GetTracksByGenreId(1);
+foreach (var track in tracksByGenre) {
+    Console.WriteLine(track.Name);
+}
